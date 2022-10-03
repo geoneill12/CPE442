9fdf2a27335f37899c9e10b9e356c1c446f2a505
student <geoneill12@gmail.com>
2022-10-02 19:23:59 -0700

    test

--- lab2/DisplayImage.cpp
+++ lab2/DisplayImage.cpp
@@ -4,25 +4,35 @@
 using namespace cv;
 using namespace std;
 
-int main(int argc, char** argv)
-{
- // Read the image file
- Mat image = imread(argv[1],1);
+int main(	int argc,
+			char** argv
+			){
 
- // Check for failure
- if (image.empty()) 
- {
-  cout << "Could not open or find the image" << endl;
-  cin.get(); //wait for any key press
-  return -1;
- }
+	// Read the image file
+	Mat image = imread(argv[1],1);
 
- String windowName = "Hawk"; //Name of the window
- namedWindow(windowName,0); // Create a window
- resizeWindow("Hawk",500,500); // Resize window
- imshow(windowName, image); // Show our image inside the created window.
- waitKey(0); // Wait for any keystroke in the window
- destroyWindow(windowName); //destroy the created window
+	// Check for failure
+	if (image.empty()){
+		cout << "Could not open or find the image" << endl;
+		cin.get(); //wait for any key press
+		return -1;
+	}
 
- return 0;
+	//Name the window
+	String windowName = "Hawk";
+ 
+	// Create a window
+	namedWindow(windowName,0);
+ 
+	// Resize the window
+	resizeWindow("Hawk",500,500);
+ 
+	// Show image inside the window
+	imshow(windowName, image);
+ 
+	// Wait for any keystroke, then destroy the created window
+	waitKey(0);
+	destroyWindow(windowName);
+
+	return 0;
 }
