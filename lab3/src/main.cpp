91a5adf49557b424f6f49b1caeb9a921d286d62b
geoneill12 <geoneill@calpoly.edu>
2022-10-25 10:00:27 -0700

    Update main.cpp

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
+#include <chrono>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
-using namespace std;
 
 Mat to442_grayscale(Mat mat_image){
-
-	uint8_t test = 0;
 
 	Mat modified(mat_image.rows, mat_image.cols, CV_8UC1);
 
@@ -23,68 +21,87 @@ Mat to442_grayscale(Mat mat_image){
 	return modified;
 }
 
+Mat to442_sobel(Mat img_g){
+
+    Mat img_s(img_g.rows, img_g.cols, CV_8UC1);
+    int p1,p2,p3,p4,p5,p6,p7,p8,p9,G;
+
+    // (0,0) is top left, increasing down and to the right
+    for(int i=1; i<(img_g.rows-1); i++){
+        for(int j=1; j<(img_g.cols-1); j++){
+            if(j==1){
+                p1 = img_g.at<uint8_t>(i-1,j-1);
+                p2 = img_g.at<uint8_t>(i-1,j);
+                p3 = img_g.at<uint8_t>(i-1,j+1);
+                p4 = img_g.at<uint8_t>(i,j-1);
+                p5 = img_g.at<uint8_t>(i,j);
+                p6 = img_g.at<uint8_t>(i,j+1);
+                p7 = img_g.at<uint8_t>(i+1,j-1);
+                p8 = img_g.at<uint8_t>(i+1,j);
+                p9 = img_g.at<uint8_t>(i+1,j+1);
+            }
+            else{
+                p1 = p2;
+                p2 = p3;
+                p3 = img_g.at<uint8_t>(i-1,j+1);
+                p4 = p5;
+                p5 = p6;
+                p6 = img_g.at<uint8_t>(i,j+1);
+                p7 = p8;
+                p8 = p9;
+                p9 = img_g.at<uint8_t>(i+1,j+1);
+            }
+            G = (abs(-p1-2*p4-p7+p3+2*p6+p9) + abs(p1+2*p2+p3-p7-2*p8-p9)) >> 3;
+            img_s.at<uint8_t>(i,j) = G;
+        }
+    }
+
+    return img_s;
+}
+
 int main(int argc, char** argv){
+
+    // Start timer
+    auto start = std::chrono::high_resolution_clock::now();
 
     VideoCapture cap(argv[1]);
     Mat img;
     Mat img_gray;
-    Mat img_sob(1080, 1920, CV_8UC1, Scalar(0));
-
-    int rows;
-    int cols;
-    int p1,p2,p3,p4,p5,p6,p7,p8,p9,G;
+    Mat img_sobel;
 
     while(true){
+
+        // Get video frame, break while loop if no more frames
         cap.read(img);
         if (img.empty()){
             break;
 	    }
-        rows = img.rows;
-        cols = img.cols;
 
-        /********** begin grayscale code **********/
+        // Run custom grayscale and sobel functions
         img_gray = to442_grayscale(img);
-        /********** end grayscale code **********/
+        img_sobel = to442_sobel(img_gray);
 
-        /********** begin sobel code **********/
-        // (0,0) is top left, increasing down and to the right
-        for(int i=1; i<(rows-1); i++){
-            for(int j=1; j<(cols-1); j++){
-                p1 = img_gray.at<uint8_t>(i-1,j-1);
-                p2 = img_gray.at<uint8_t>(i-1,j);
-                p3 = img_gray.at<uint8_t>(i-1,j+1);
-                p4 = img_gray.at<uint8_t>(i,j-1);
-                p5 = img_gray.at<uint8_t>(i,j);
-                p6 = img_gray.at<uint8_t>(i,j+1);
-                p7 = img_gray.at<uint8_t>(i+1,j-1);
-                p8 = img_gray.at<uint8_t>(i+1,j);
-                p9 = img_gray.at<uint8_t>(i+1,j+1);
-                G = (abs(-p1-2*p4-p7+p3+2*p6+p9) + abs(p1+2*p2+p3-p7-2*p8-p9)) >> 3;
-                img_sob.at<uint8_t>(i,j) = G;
-            }
-        }
-        /********** end sobel code **********/
-
+        // Display image windows
         namedWindow("Sobel", 0);
         namedWindow("Color", 0);
-
-        resizeWindow("Sobel",cols,rows);    // 500,300
+        resizeWindow("Sobel",img_sobel.cols,img_sobel.rows);    // 500,300
         resizeWindow("Color",500,300);   // 500,300
-
         moveWindow("Sobel", 0,0);  // 1000,0
         moveWindow("Color", 0,0);    // 500,0
-
-        imshow("Sobel", img_sob);
+        imshow("Sobel", img_sobel);
         imshow("Color", img);
 
+        // Stop program is "ESC" is pressed
         char c=(char)waitKey(1);
         if(c==27){
             break;
         }
     }
 
-    //cout << rows << "\n";
-    //cout << cols << "\n";
+    // Display timing results
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::cout << duration.count() << "\n";
 
 	return 0;
 }
